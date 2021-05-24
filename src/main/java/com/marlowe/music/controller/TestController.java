0896803cc2674189d0f33d4ba20209565fe4d90f
XMMarlowe <2531649481@qq.com>
2021-05-24 18:35:29 +0800

    初始化提交 测试TestController

--- src/main/java/com/marlowe/music/controller/TestController.java
+++ src/main/java/com/marlowe/music/controller/TestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "hello world";
+        return "Hello World";
     }
 
 }
