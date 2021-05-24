ede783d2d407e6cda0450e37d112692d66acf7a9
XMMarlowe <2531649481@qq.com>
2021-05-24 18:33:49 +0800

    初始化提交 测试TestController

---
+++ src/main/java/com/marlowe/music/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.marlowe.music.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: music
+ * @description: test
+ * @author: Marlowe
+ * @create: 2021-05-24 17:31
+ **/
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "hello world";
+    }
+
+}
---
+++ src/test/java/com/marlowe/music/MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlowe.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
